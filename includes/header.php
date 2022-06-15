dd5e3e2a8857edc5c7e6ba8806d125cb6e8a963f
dharunraj07 <59676982+dharunraj07@users.noreply.github.com>
2022-06-15 23:36:39 +0530

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,51 @@
+<header class="header-desktop">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="header-wrap">
+                            <div class="header-button">
+                                
+                                <div class="account-wrap">
+                                    <div class="account-item clearfix js-item-menu">
+                                        <div class="image">
+                                            <img src="images/icon/avatar.jpg" alt="Admin" />
+                                        </div>
+                                        <div class="content">
+                                            <a class="js-acc-btn" href="admin-profile.php"><?php echo $name; ?></a>
+                                        </div>
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                <div class="image">
+                                                    <a href="#">
+                                                        <img src="images/icon/avatar.jpg" alt="Admin" />
+                                                    </a>
+                                                </div>
+                                                <div class="content">
+                                                    <h5 class="name">
+                                                        <a href="#"><?php echo $name; ?></a>
+                                                    </h5>
+                                                   
+                                                </div>
+                                            </div>
+                                            <div class="account-dropdown__body">
+                                                <div class="account-dropdown__item">
+                                                    <a href="admin-profile.php">
+                                                        <i class="zmdi zmdi-account"></i>Admin Profile</a>
+                                                </div>
+                                                <div class="account-dropdown__item">
+                                                    <a href="change-password.php">
+                                                        <i class="zmdi zmdi-settings"></i>Change Password</a>
+                                                </div>
+                                               
+                                            </div>
+                                            <div class="account-dropdown__footer">
+                                                <a href="logout.php">
+                                                    <i class="zmdi zmdi-power"></i>Logout</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
